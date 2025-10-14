033b4435c5c46c7ebfb08737f7dede75c9b5da5a
MBerkayYurumez <muhammetberkayyurumez@gmail.com>
2025-10-14 11:58:17 +0300

    Add files via upload

---
+++ NeesneTabanliDers2/NeesneTabanliDers2/Program.cs
@@ -0,0 +1,229 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NeesneTabanliDers2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //matematiksel işlem yapılırken, int veya daha büyük bir türe dönüştürmek gerekir.
+            //(en az giren veriler kadar büyük olması gerekir.)
+
+            byte a = 5, b = 10;
+            short d = 2, e = 9;
+            byte f = a + b;
+            short g = d + e;
+            Console.WriteLine(g);
+            */
+
+
+            /*
+            int a = 10;
+            byte b = 20;
+            short c = 30;
+            double d = a + b + c;
+            Console.WriteLine(d);
+            */
+
+
+            /*
+            //bir karakter inte dönüşürken, ascii tablosundaki karşılığının değerini alır.
+            double c = 16.5f;
+            int d = 'a';
+            Console.WriteLine(d);
+            Console.WriteLine(c);
+            */
+
+
+            /*
+            //Hatalı bir dönüştürmedir.
+            decimal c = 10;
+            byte b = c;
+            char d = c;
+            Console.WriteLine(d);
+            */
+
+
+            /*
+            const double pi = Math.PI;
+            Console.WriteLine(pi);
+            int tamsayi = (int)pi;  //double -> int  , kesir kısım atılır.
+            Console.WriteLine(tamsayi);
+            */
+
+
+            
+            int i = 25;
+            byte b = (byte)i;
+            Console.WriteLine(b);
+
+            int c = 2561;
+            byte d = (byte)c;
+            Console.WriteLine(d);
+            
+
+            
+            /*
+            //Taşma olacağı için hata verir.
+            int i = 256;
+            byte b;
+            //checked bloğunun içinde yapılan atama dışarıda kullanılamayacağı için atamayı dışarıda yapıyoruz.
+            
+            checked
+            {
+                b = (byte)i;
+            }
+            Console.Write(b);
+            */
+
+
+            //SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+            /*
+            int i = 256;
+            int a = 300;
+            byte b, c;
+            checked
+            {
+                b = (byte)i;
+                unchecked
+                {
+                    c = (byte)a;
+                }
+            }
+            Console.WriteLine(b);
+            */
+
+
+            /*
+            try
+            {
+                string s1, s2;
+
+                int sayi1, sayi2;
+                int Toplam;
+
+                Console.WriteLine("İlk Sayıyı Gir: ");
+                s1 = Console.ReadLine();
+                sayi1 = Convert.ToInt32(s1);
+                //sayi1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("İkinci Sayıyı Gir: ");
+                s2 = Console.ReadLine();
+                sayi2 = Convert.ToInt32(s2);
+                Toplam = sayi1 + sayi2;
+                Console.WriteLine("Toplam = " + Toplam.ToString());
+            }
+            catch
+            {
+                Console.WriteLine("Yanlış veri tipi! Programı kapatıp tekrar deneyiniz.");
+            }
+            */
+
+
+
+            /*
+            Console.WriteLine (3.ToString ());
+
+            string str = 36.6f.ToString();
+            Console.WriteLine(str);
+
+            bool b = false;
+            int i = Convert.ToInt32(b);
+            Console.WriteLine(i);
+
+            int a = 123;
+            bool bo = Convert.ToBoolean(a);
+            Console.WriteLine(bo);
+            int c = 0;
+
+            bool d = Convert.ToBoolean (c);
+            Console.WriteLine(d);
+
+            */
+
+
+            /*
+            int a = 5;
+            char m = 'k';
+            string r = "deneme";
+            float f = 12.5f;
+            string b = a + m + r + f;
+            Console.WriteLine(b);
+            */
+
+
+            /*
+            //hata verir, toplamada string bir veri kullanılmadığı için
+            int a = 5;
+            char m = 'a';
+            float f = 12.5f;
+            string b = a + m + f;
+            Console.WriteLine(b);
+            */
+
+
+            /*
+            int a = 10;
+            int b = 20;
+            int c = 30;
+            int d = 40;
+            float f = 5.6f;
+
+            a++;
+            ++b;
+            c--;
+            --d;
+            f++;
+
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n",
+                a,b,c,d,f);
+            */
+
+
+            /*
+            int a = 10;
+            int b;
+            int c;
+            b = a++;//b=10 değeri verilir, a nın değeri artırılır(11 olur).
+            c = ++a;//a nın değeri artırılır(12 olur), c ye bu değeri atar.
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            */
+
+            /*
+            bool b1 = 60 < 50;
+            bool b2 = 50 < 50;
+            bool b3 = b2;
+            bool b4 = b3;
+            Console.WriteLine("{0}\n{1}\n{2}\n{3}\n", b1,b2,b3,b4);
+            */
+
+
+            /*
+            object i = "50";
+            string s = i as string;
+            Console.WriteLine(s);
+            */
+
+
+            /*
+            int i = 50;
+            bool b1 = i is int;
+            bool b2 = i is string;
+            bool b3 = i is object;
+
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.WriteLine(b3);
+            */
+
+
+
+
+        }
+    }
+}
---
+++ NesneTabanliders1/NesneTabanliders1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System.Net.Security;
+
+namespace NesneTabanliders1
+{
+    class Program1()
+    {
+        static int a = 10;
+        static void Main()
+        {
+            /*
+            Console.WriteLine("Hello World!");
+            */
+
+
+            /*
+            Console.WriteLine("Bir tuşa basınız.");
+            Console.ReadKey();
+            Console.WriteLine("Bir tuşa bastınız.");
+            */
+
+            //büyük küçük harf fark eder
+            /*
+            int ucret = 10;
+            int UCRET;
+            UCRET = 50;
+            Console.WriteLine(ucret);
+            Console.WriteLine(UCRET);
+            */
+
+            //dış blokta atama yaptıktan sonra iç blokta aynı isimle atama yapamazsın
+            /*
+            int a;
+            {
+                int a = 10;
+            }
+            */
+
+
+            //main dışında yapılan statik atama etkilemez
+            /*
+            int a= 5;
+            Console.WriteLine(a);
+            */
+
+
+            //sabit değer atamak için
+            /*
+            const double pi = 3.14;
+            pi = 3.1;
+            */
+
+            //c sabit bir değer olduğu için hatalı bir atamadır.
+            /*
+            int a = 3, b;
+            b = a;
+            const int c = b + a;
+            */
+
+            //hatalı bir atamadır.
+            /*
+            const int a = 5;
+            const int b = a + 6;
+            Console.WriteLine(b);
+            */
+
+            //hatalı bir atamadır.
+            /*
+            const int a = 3;
+            a=a + 1;
+            */
+            /*
+            int tamsayi = new int();
+            int tamsayi1 = 0;
+            Console.WriteLine(tamsayi);
+            Console.WriteLine(tamsayi1);
+            */
+
+            /*
+            int a = new int();
+            char b = new char();
+            bool c = new bool();
+            float f  =new float();
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(f);
+            */
+            /*
+            byte b = 0;
+            byte a = 10;
+            byte c = 255;
+            */
+            /*
+            float f = 3.12f;
+            decimal j = 3.12m;
+            Console.WriteLine(f);
+            */
+            /*
+            bool b1 = true;
+            bool b2 = false;
+            int a = 50;
+
+            bool b3 = a < 60;
+
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.WriteLine(b3);
+            */
+            /*
+            char c1 = 'A';
+            char c2 = (char)90;
+            char c3 = '\u0058';
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+            */
+
+            /*
+            string s1 = "Hello";
+            string s2 = ".NET";
+            string s3 = s1 + s2;
+            Console.WriteLine(s3);
+            */
+            string s4 = "Escape karakterleri \',\\";
+            string s5 = @"Escape karakterleri ',\";
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.ReadLine();
+
+        }
+    }
+}
